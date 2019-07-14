2b8a865754a6c1361720b62b8d3d63b8f23b1e97
chino <chino618@me.com>
2019-07-14 11:56:15 +0900

    Ready for Lesson2.

--- Planets/main.swift
+++ Planets/main.swift
@@ -1,8 +1,8 @@
 //
 //  main.swift
 //  Planets
-//
-//  Created by 新井進鎬 on 2019/07/12.
+//  Lesson2
+//  Created by imchino on 2019/07/12.
 //  Copyright © 2019 chino. All rights reserved.
 //
 
@@ -10,29 +10,9 @@ import Foundation
 
 print("This is planets App.")
 
-// TODO: プロジェクト基本
-// プロジェクトナビゲータを使って、main.swift を開く。
-// プログラムを実行 (⌘R) する
-// コンソール (⇧⌘C) を観察して、プログラムの結果を確認する
-
-
-// TODO: 定数を宣言する
-// 2つの定数 `numberOfPlanets` と `diameterOfEarth` を宣言する
-//let numberOfPlanets = 8 
-//let diameterOfEarth = 12756.274 // In km, from pole to pole.
 let numberOfPlanets = 8
 let diameterOfEarth = 12756.274 // In km, from pole to pole.
 
-
-// TODO: 文字列にオブジェクトを埋め込む
-// Hello world の出力を "This is Planets App." に変更
-// 変数の宣言以降の行で、自分なりに print() 呼び出しをいくつか追加する
-//print("There are \(numberOfPlanets) planets in the sorlar system.")
-//print("Earth has a circumference of \(diameterOfEarth) miles.")
 print("There are \(numberOfPlanets) planets in the sorlar system.")
 print("Earth has a circumference of \(diameterOfEarth) miles.")
 
-
-// TODO: 実行して、出力結果を確認する
-// プログラムを実行する
-// コンソール出力を観察する
