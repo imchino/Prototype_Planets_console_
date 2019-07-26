996959d1489337f4a1237117ddd79b589b628f89
chino <chino618@me.com>
2019-07-26 15:45:57 +0900

    mintor edit.

--- Planets/GaraxyKnowledge.swift
+++ Planets/GaraxyKnowledge.swift
@@ -7,8 +7,37 @@
 //
 
 import Foundation
+
 struct GalaxyKnowledge {
     func search() {
-        
+        print("This is planets App.")
+
+        let numberOfPlanets = 8
+        let diameterOfEarth = 12756.274 // In km.
+
+        print("There are \(numberOfPlanets) planets in the sorlar system.")
+        print("Earth has a circumference of \(diameterOfEarth) miles.")
+
+        print("Let's learn about the planet.")
+        print("Shall I randomly select a planet to search? (Y or N)")
+
+        var isRandomlySelected: String?
+        var isInvalidInput: Bool {
+            // ユーザ入力が無効なら true を返す
+            return !(isRandomlySelected == "Y" || isRandomlySelected == "N")
+        }
+
+        while isInvalidInput {
+            isRandomlySelected = readLine()
+            if isRandomlySelected! == "Y" {
+                print("OK! Searching for...")
+                // TOOD: travel to random planet 
+            } else if isRandomlySelected! == "N" {
+                print("OK, Tell the planet you would like to know....")
+                // TODO: let the user select a planet to visit 
+            } else {
+                print("Please input Y or N, again.")
+            }
+        }
     }
 }
